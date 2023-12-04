99cf52447f9d385785768c70b58de9bdaf36189f
chas-e <charles@chasengineering.dev>
2023-12-04 06:52:33 -0500

    🫧 Yarn, prettier config

---
+++ .prettierrc.mjs
@@ -0,0 +1,22 @@
+export default {
+    plugins: ['prettier-plugin-svelte', 'prettier-plugin-astro'],
+    overrides: [
+        {
+            files: '*.svelte',
+            options: {
+                parser: 'svelte',
+            },
+        },
+        {
+            files: '*.astro',
+            options: {
+                parser: 'astro',
+            },
+        },
+    ],
+    printWidth: 120,
+    semi: true,
+    singleQuote: true,
+    tabWidth: 4,
+    trailingComma: "es5"
+};
